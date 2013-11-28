7c50b14fc8d8972fe2b881db44c781d3fb24e74d
Orlov Lronid <feonitu@yandex.ru>
2013-11-29 01:23:40 +0400

    добавил доки, допилил запросы

--- task2/script.js
+++ task2/script.js
@@ -1,119 +1,190 @@
-/**
+/*
+ * Решение на тестовое задание Anywayanyday "Поиск рейсов авиакомпаний"
  *
+ * author: Orlov Leonid
+ * email: feonitu@yandex.ru
  * */
 
-var app;
 
-!function($, global){
 
-	global.app = {};
+var app = new function AirLine() {
 
-	app.urlTemplates = {
-		start : 'http://api.anywayanyday.com/api/NewRequest/?Route=2406MOWLON&AD=1&CN=0&CS=E&Partner=testapic&_Serialize=JSON',
-		status : 'http://api.anywayanyday.com/api/RequestState/?R=${ID}&_Serialize=JSON',
-		result : 'http://api.anywayanyday.com/api/Fares/?R=${ID}&V=Matrix&VB=true&L=ru&_Serialize=JSON'
-	};
+  /**
+   * Хранилище
+   * */
 
-	app.send = function(url, handle){
-		$.ajax({
-			url : url,
-			type : 'GET',
-			dataType: 'jsonp',
-			crossDomain : true
-		})
-			.done(function(data){
-				if (!data) {
-		      return alert('Не полученны данные');
-	      }
+  this.store = null;
 
-	      if (data['Error'] !== null) {
-		      return alert('Ошибка: ' + data['Error']);
-	      }
-	      console.log(data);
-	      return handle(data);
-			})
-			.fail(function(jqXHR, textStatus, errorThrown) {
-		    alert('ajax fail: ' + errorThrown);
-	    });
-	};
-	
-		app.handle = function(data){
-		var id = data.Id,
-		    init;
-		    
-		init = setInterval(function(){
-		  app.requestStatus(id, function(data){
-		    if (data.Completed == 100){
-		      clearInterval(init);
-		      app.requestResult(id, function(data){
-		        app.appendData(data)
-		      })
-		    }
-		  })
-		}, 1000);
-	};
+  //==============================================================================================
+  //      Получение данных
+  //==============================================================================================
 
-	app.requestStart = function(){
-		var url = app.urlTemplates.start;
+  /**
+   * Шаблоны по предоставленному API сервиса, производящего поиск рейсов авиакомпаний
+   *
+   * */
 
-		app.send(url, app.handle)
-	};
+  this.urlTemplates = {
+    start : 'http://api.anywayanyday.com/api/NewRequest/?Route=2406MOWLON&AD=1&CN=0&CS=E&Partner=testapic&_Serialize=JSON',
+    status : 'http://api.anywayanyday.com/api/RequestState/?R=${ID}&_Serialize=JSON',
+    result : 'http://api.anywayanyday.com/api/Fares/?R=${ID}&V=Matrix&VB=true&L=ru&_Serialize=JSON'
+  };
 
-	app.requestStatus = function(id, handle){
-		var url = $.tmpl( app.urlTemplates.status, {
-				ID : id
-			}).text();
+  /**
+   * Запрос к сервису
+   *
+   * @param {String} url Адрес ресурса по предоставленному API
+   * @param {Function} handle Обработчик ответа
+   * */
 
-		app.send(url, handle)
-	};
+  this.sendGetRequest = function(url, handle){
 
-	app.requestResult = function(id, handle){
-		var url = $.tmpl( app.urlTemplates.result, {
-				ID : id
-			}).text();
+    $.ajax({
+      url : url,
+      type : 'GET',
+      dataType: 'jsonp',
+      crossDomain : true
+    })
 
-		app.send(url, handle)
-	};
-
-app.appendData = function(data){
-  var airLines = data.Airlines,
-      $divConteiner = $('.left'),
-      $html = $('<ol>'),
-      template = "<li><a class='folder-close' href= '#'>${name}</a></li>",
-      
-      len = airLines.length;
-      
-      
-      for(var i=0; i<len; i+=1){
-        $.tmpl(template, {
-                           name : airLines[i].Name
-                         }
-        ).appendTo($html);
-      };
-      
-      app.appendFlightData(airLines[2]);
-      $divConteiner.append($html);
-}
-
-app.appendFlightData = function(airLines){
-  var fares = airLines.FaresFull,
-      $divConteiner = $('.center'),
-      template = "<div> ${id} ${amount} ${seats} </div>",
-      len = fares.length,
-      $html = $('<div>');
-      
-      for (var i=0; i < len; i+=1){
-        data = {
-         id : fares[i].FareId,
-         seats : fares[i].MinAvailSeats,
-         amount : fares[i].TotalAmount,
-       }
-      
-        $.tmpl(template, data).appendTo($html)
+    .done( function(data) {
+      if (!data) {
+        return alert('Не получены данные от сервиса!');
       }
 
-      $divConteiner.append($html);
-}
+      if (data['Error'] && data['Error'] !== null) {
+        return alert('Сервис прислал ошибку: ' + data['Error']);
+      }
+      console.log(data);
+      return handle(data);
+    })
+
+    .fail(function (jqXHR, textStatus, errorThrown) {
+      alert('Запрос через AJAX был неудачен: ' + errorThrown);
+    });
+
+  };
+
+  /**
+   * Запуск поиска
+   *
+   * @this {AirLine}
+   * */
+
+  this.requestStart = function(){
+    var url = this.urlTemplates.start,
+      that = this;
+
+    this.sendGetRequest(url, function (data) {
+      var id;
+
+      if ( id = data['Id'] ) {
+        that.processSearch(id);
+      }
+    })
+  };
+
+  /**
+   * Обработка поиска
+   *
+   * @this {AirLine}
+   * */
+
+  this.processSearch = function(id){
+    var init,
+      that = this;
+    // Периодически
+    init = setInterval(function(){
+      // Опрашиваем статус
+      that.requestStatus(id, function(data){
+        // Пока не убедимся, что поиск завершён
+        if (data['Completed'] && data['Completed'] == 100){
+          // После чего останавливаем счетчик
+          clearInterval(init);
+          // Получаем результат поиска
+          that.requestResult(id, function(data){
+            // И обрабатываем данные
+            that.appendData(data)
+          })
+        }
+      })
+    }, 1000);
+  };
 
 
-}(jQuery, this);
+  /**
+   * Для получения статуса поиска
+   *
+   * @this {AirLine}
+   * @param {String} id Идентификатор поискового запроса
+   * @param {Function} handle Обработчик результата
+   * */
+
+	this.requestStatus = function(id, handle){
+		var temp = this.urlTemplates.status,
+        url = $.tmpl( temp, {	ID : id }).text();
+
+		this.sendGetRequest(url, handle);
+	};
+
+  /**
+   * Для получения результата поиска
+   *
+   * @this {AirLine}
+   * @param {String} id Идентификатор поискового запроса
+   * @param {Function} handle Обработчик результата
+   * */
+	this.requestResult = function(id, handle){
+		var temp = this.urlTemplates.result,
+        url = $.tmpl( temp, { ID : id}).text();
+
+		this.sendGetRequest(url, handle);
+	};
+
+
+  //==============================================================================================
+  //      Обработка и отображение данных
+  //==============================================================================================
+
+  this.appendData = function(data){
+    var airLines = data.Airlines,
+        $divConteiner = $('.left'),
+        $html = $('<ol>'),
+        template = "<li><a class='folder-close' href= '#'>${name}</a></li>",
+
+        len = airLines.length;
+
+
+        for(var i=0; i<len; i+=1){
+          $.tmpl(template, {
+                             name : airLines[i].Name
+                           }
+          ).appendTo($html);
+        };
+
+        app.appendFlightData(airLines[2]);
+        $divConteiner.append($html);
+  }
+
+  this.appendFlightData = function(airLines){
+    var fares = airLines.FaresFull,
+        $divConteiner = $('.center'),
+        template = "<div> ${id} ${amount} ${seats} </div>",
+        len = fares.length,
+        $html = $('<div>');
+
+        for (var i=0; i < len; i+=1){
+          data = {
+           id : fares[i].FareId,
+           seats : fares[i].MinAvailSeats,
+           amount : fares[i].TotalAmount
+         };
+
+          $.tmpl(template, data).appendTo($html)
+        }
+
+        $divConteiner.append($html);
+  };
+
+  this.requestStart()
+
+};
